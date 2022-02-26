ebcb2c45bc25dd80d17a46a84398eb9af514e4df
Otorbaev Adilet <adilet_otor@mail.ru>
2022-02-26 18:56:33 +0600

    work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        OblastKR oblastKR1 = OblastKR.Chuy;
-        //System.out.println(OblastKR.Chuy);
+        System.out.println(DniNedeli.Monday);
+        System.out.println(DniNedeli.Tuesday);
+        System.out.println(DniNedeli.Wednesday);
+        System.out.println(DniNedeli.Thursday);
+        System.out.println(DniNedeli.Friday);
+        System.out.println(DniNedeli.Saturday);
+        System.out.println(DniNedeli.Sunday);
+
+        // Online work --->
+//        OblastKR oblastKR1 = OblastKR.Chuy;
+//        System.out.println(OblastKR.Chuy);
+
 //        OblastKR oblastKR1 = OblastKR.Talas;
 //        OblastKR oblastKR1 = OblastKR.Naryn;
 //        OblastKR oblastKR1 = OblastKR.Osh;
@@ -13,14 +23,14 @@ public class Main {
 //        OblastKR oblastKR1 = OblastKR.Batken;
 //        OblastKR oblastKR1 = OblastKR.IssykKul;
 
-        switch (oblastKR1){
-            case Chuy -> System.out.println("\nАдминистративным центром Чуйской области является город: "+oblastKR1.getGorod()+ "\n"+OblastKR.Chuy.getOsobennosti());
-            case Talas -> System.out.println("\nАдминистративным центром Таласской области является город: "+oblastKR1.getGorod()+"\n"+OblastKR.Talas.getOsobennosti());
-            case Naryn -> System.out.println("\nАдминистративным центром Нарынской области является город: "+oblastKR1.getGorod()+"\n"+OblastKR.Naryn.getOsobennosti());
-            case Osh -> System.out.println("\nАдминистративным центром Ошской области является город: "+oblastKR1.getGorod()+"\n"+ oblastKR1.Osh.getOsobennosti());
-            case DjalalAbad -> System.out.println("\nАдминистративным центром Джалал-Абадской области является город: "+oblastKR1.getGorod()+"\n"+oblastKR1.getOsobennosti());
-            case Batken -> System.out.println("\nАдминистративным центром Баткенской области является город: "+oblastKR1.getGorod()+"\n"+oblastKR1.getOsobennosti());
-            case IssykKul -> System.out.println("\nАдминистративным центром Иссык-Кульской области является город: "+oblastKR1.getGorod()+"\n"+oblastKR1.getOsobennosti());
-        }
+//        switch (oblastKR1){
+//            case Chuy -> System.out.println("\nАдминистративным центром Чуйской области является город: "+oblastKR1.getGorod()+ "\n"+OblastKR.Chuy.getOsobennosti());
+//            case Talas -> System.out.println("\nАдминистративным центром Таласской области является город: "+oblastKR1.getGorod()+"\n"+OblastKR.Talas.getOsobennosti());
+//            case Naryn -> System.out.println("\nАдминистративным центром Нарынской области является город: "+oblastKR1.getGorod()+"\n"+OblastKR.Naryn.getOsobennosti());
+//            case Osh -> System.out.println("\nАдминистративным центром Ошской области является город: "+oblastKR1.getGorod()+"\n"+ oblastKR1.Osh.getOsobennosti());
+//            case DjalalAbad -> System.out.println("\nАдминистративным центром Джалал-Абадской области является город: "+oblastKR1.getGorod()+"\n"+oblastKR1.getOsobennosti());
+//            case Batken -> System.out.println("\nАдминистративным центром Баткенской области является город: "+oblastKR1.getGorod()+"\n"+oblastKR1.getOsobennosti());
+//            case IssykKul -> System.out.println("\nАдминистративным центром Иссык-Кульской области является город: "+oblastKR1.getGorod()+"\n"+oblastKR1.getOsobennosti());
+//        }
     }
 }
